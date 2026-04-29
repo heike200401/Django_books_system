ebf29f931dd2c9a5764c5efae9258a8f0f35c606
heike200401 <2054979516@qq.com>
2026-04-29 09:44:03 +0800

    Delete library directory

--- library/models.py
+++
@@ -1,5 +0,0 @@
-# 导入Django数据库模型模块
-from django.db import models
-
-# 此文件用于定义library应用的数据模型
-# 当前为空，可在此添加自定义模型类
--- library/views.py
+++
@@ -1,5 +0,0 @@
-# 导入Django快捷函数
-from django.shortcuts import render
-
-# 此文件用于定义library应用的视图函数
-# 当前为空，可在此添加自定义视图函数
